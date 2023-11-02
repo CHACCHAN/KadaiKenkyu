fecf35e1e6cc76041cfaef8fdaddf0b7f05098c8
CHACCHAN <chacchan.info@gmail.com>
2023-11-02 16:55:40 +0900

    カードを増やした

--- resources/views/Profile/account.blade.php
+++ resources/views/Profile/account.blade.php
@@ -1,4 +1,12 @@
 @extends('Profile.profile')
+@section('CSS')
+<style>
+#CardHover:hover {
+    background: rgb(231, 231, 231);
+    transition: 0.3s;
+}
+</style>
+@endsection
 @section('view')
 <div class="container-fluid">
     <div class="row">
@@ -15,30 +23,62 @@
             <div class="mt-3">
                 <div class="container">
                     <div class="row px-5">
-                        <div class="col-6">
-                            <div class="card">
-                                <div class="card-body">
-
-                                </div>
-                                <div class="card-footer">
-                                    <div class="text-primary py-1">
-                                        メールアドレスを変更する
+                        @php
+                            // カードタイトル
+                            $CardTitle = array(
+                                1 => 'メールアドレス',
+                                2 => 'CHaserOnline',
+                                3 => 'パスワード'
+                            );
+                            // カードコンテンツ
+                            $CardContent = array(
+                                1 => '自身のメール状況を確認できます。メールアドレスの編集と更新はこちらから',
+                                2 => 'CHaserOnlineの情報と設定情報を確認できます。CHaserOnlineアカウントの更新はこちらから',
+                                3 => '自身のパスワードを確認できます。パスワードの編集と更新はこちらから'
+                            );
+                            // カードフッター
+                            $CardFooter = array(
+                                1 => 'メールアドレスを変更する',
+                                2 => 'CHaserOnlineを設定する',
+                                3 => 'パスワードを変更する',
+                            );
+                            // カードイメージ
+                            $CardImage = array(
+                                1 => '',
+                                2 => '',
+                                3 => '',
+                            );
+                            $MAX = 3;
+                        @endphp
+                        {{-- カード --}}
+                        @for($i=1;$i<=$MAX;$i++)
+                            <div class="col-6">
+                                <div class="mb-3">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-8">
+                                                    <div class="card-title h5">
+                                                        {{ $CardTitle[$i] }}
+                                                    </div>
+                                                    <div class="card-text">
+                                                        {{ $CardContent[$i] }}
+                                                    </div>
+                                                </div>
+                                                <div class="col-4">
+    
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div id="CardHover" class="card-footer bg-light">
+                                            <div class="text-primary py-1">
+                                                {{ $CardFooter[$i] }}
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="card">
-                                <div class="card-body">
-
-                                </div>
-                                <div class="card-footer">
-                                    <div class="text-primary py-1">
-                                        CHaserOnlineを設定する
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        @endfor
                     </div>
                 </div>
             </div>
--- resources/views/Profile/profile.blade.php
+++ resources/views/Profile/profile.blade.php
@@ -6,7 +6,7 @@
         {{-- メニュー --}}
         <div class="col-3">
             <div class="mt-3">
-                <div class="list-group">
+                <div class="list-group list-group-flush">
                     <a href="{{ route('Profile.account') }}" class="list-group-item list-group-item-action @if(Request::is('profile/account')) @php echo 'list-group-item-secondary' @endphp @endif">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
