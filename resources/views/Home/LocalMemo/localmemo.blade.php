cd2beca3eff1e61e48861a27dca550c0151a8bf0
CHACCHAN <chacchan.info@gmail.com>
2023-11-03 14:54:14 +0900

    修正

--- resources/views/Home/LocalMemo/localmemo.blade.php
+++ resources/views/Home/LocalMemo/localmemo.blade.php
@@ -2,10 +2,6 @@
 @section('title', 'ローカルメモ')
 @section('content')
 <div class="container-fluid">
-<<<<<<< HEAD
 
-=======
-    aaaaaaa
->>>>>>> 1d400a031b23aa215682483e53dcaf4093410e58
 </div>
 @endsection
